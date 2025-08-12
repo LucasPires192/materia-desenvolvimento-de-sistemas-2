ed794e30c410997b547ab30548692d3b3223a5b0
LucasPires192 <lucas.andrade192@etec.sp.gov.br>
2025-08-12 20:35:36 -0300

    feat: exercicio de revisao do javascript da aula 01

---
+++ aula01/main.js
@@ -0,0 +1,32 @@
+function somar(a, b){
+	return a + b;
+}
+
+function subtrair(a, b){
+	return a - b;
+}
+
+const lista = [1, 2, 3, 4];
+
+const lista2 = [
+    {
+        nome: "Lucas",
+        idade: 29
+    },
+    {
+        nome: "Maria",
+        idade: 19
+    },
+    {
+        nome: "João",
+        idade: 32
+    },
+]
+
+console.log("Olá Mundo");
+
+console.log(somar(2, 2));
+console.log(subtrair(10, 7));
+
+console.log(lista);
+console.log(lista2);
